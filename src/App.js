c5742d7239046a2b6a73bbfbff3691aa546670d4
Ertug Dilek <ertgdlk@gmail.com>
2020-11-04 12:00:32 +0300

    appjs refactor

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 //import logo from './logo.svg';
 //import './App.css';
 import React from 'react';
-import Navbar from './components/Navbar';
-import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
+import Navbar from './Components/Navbar';
+
 
 function App() {
   return (
     <>
-    <Router>
-      <Navbar/>
-        <Switch>
-          <Route path ='' exact />
-        </Switch>
-      </Router>
+    <Navbar></Navbar>
    </>
   );
 }
