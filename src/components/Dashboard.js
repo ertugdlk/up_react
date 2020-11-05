843cacfcd9dd89cd8d99e0e51182b67c0d27b52f
Ertug Dilek <ertgdlk@gmail.com>
2020-11-05 22:08:27 +0300

    Last changes dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,28 +1,52 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { createGlobalStyle } from 'styled-components'
 import Logo from '../logo.png'
 import {Grid , Button , Container} from '@material-ui/core'
 import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
 import IconButton from '@material-ui/core/IconButton';
+import {Menu, MenuItem} from '@material-ui/core'
 
 function Dashboard() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
     return(
         <>
+        <GlobalStyle></GlobalStyle>
         <Header>
           <Grid zIndex={999} >
             <LogoSize>
             <img src={Logo}/>
             </LogoSize>
+            <div style={{alignSelf:"center"}}>
             <Avatar></Avatar>
             <ArrowMenu>
-              <IconButton style={{ color: 'white' }} aria-label="menu" size="small">
+              <IconButton style={{ color: 'white' }} aria-label="menu" size="small" aria-haspopup="true" onClick={handleClick}>
                 <ArrowDownwardIcon fontSize="inherit" />
               </IconButton>
-            </ArrowMenu>  
+            </ArrowMenu> 
+            </div> 
+             <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>My account</MenuItem>
+                <MenuItem onClick={handleClose}>Logout</MenuItem>
+            </Menu>
           </Grid>
         </Header>
 
-        <Menu>
+        <MenuBar>
           <Grid
               container
               direction="column"
@@ -30,16 +54,24 @@ function Dashboard() {
               alignItems="center">
           </Grid>
           <PlayButton>Play</PlayButton>
-        </Menu> 
+        </MenuBar> 
         </>
     )
 }
 
-const Menu = styled.div`
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color:#19191f;
+  }
+`
+
+const MenuBar = styled.div`
   width: 150px;
   height: 100%;
   position: absolute;
   top:70px;
+  left:0px;
   align-self:flex-start;
   justify-self:flex-start;
   background-color: #16161b;
@@ -48,7 +80,9 @@ const Menu = styled.div`
 const Header = styled.div`
   height: 70px;
   background-color: #16161b;
-  top:0px;
+  top:0;
+  position:absolute;
+  left:0;
   width:100%;
   z-index:998;
 `;
@@ -63,8 +97,10 @@ const LogoSize = styled.div`
 const PlayButton = styled.button`
     width: 150px;
     height: 50px;
+    z-index:1000;
     background-color: #00ff60;
     border: none;
+    position:absolute;
     color: #000000;
     font-size: 17px;
     font-weight: 400;
@@ -86,10 +122,9 @@ const Avatar = styled.div`
   border: 3px solid #00ff60;
 `
 const ArrowMenu = styled.div`
-  right:120px;
-  justify-content:center;
+  top:20px;
+  right:60px;
   position:absolute;
 `
-
 
 export default Dashboard
