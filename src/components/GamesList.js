672f384184592f830b675f21755b88e80ff50270
Mert Mungan <business.mertmungan@gmail.com>
2020-11-25 16:02:02 +0300

    cards modified

--- src/components/GamesList.js
+++ src/components/GamesList.js
@@ -1,47 +1,39 @@
-import React, {useState} from 'react'
-import styled, {createGlobalStyle} from 'styled-components'
+import React, {useEffect} from 'react'
 import ClearIcon from '@material-ui/icons/Clear'
 import Logo from '../logo.png'
 import css from '../components/css/GamesList.css'
-import { Photo } from '@material-ui/icons'
-
-const GlobalStyle = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css?family=Raleway');
-  body {
-    font-family: 'Raleway', sans-serif;
-  }`
+const Axios = require('axios')
 
 
-function GamesList(props) 
-{
+function GamesList(props) {
+const [games, setGames] = React.useState([])
+
+  useEffect(()=> {
+    async function GameCards(){
+      const url = "http://localhost:5000//detail/allgames"
+      const response = await Axios.get(url, {withCredentials:true})
+        setGames(response.data)
+    }
+    
+
+    GameCards()
+  }, [])
 
     return(
         <>
-           <GlobalStyle></GlobalStyle>
           <div className='GamesList'>
             <div className='CloseButton1'> <ClearIcon fontSize='large' onClick={props.onClose}></ClearIcon> </div>
             <div class="wrapper">
 
 <div className="cards">
-
-  <figure className="card">
-
-    <img src="https://steamcdn-a.akamaihd.net/steam/apps/730/library_600x900.jpg?t=1604621079" />
-
-  </figure>
-
-  <figure className="card">
-
-    <img src="https://steamcdn-a.akamaihd.net/steam/apps/252950/library_600x900.jpg?t=1600797256" />
-
-  </figure>
-
-  <figure className="card">
-
-    <img src="https://steamcdn-a.akamaihd.net/steam/apps/578080/library_600x900.jpg?t=1603246099" />
-
-  </figure>
-
+<div className="card">
+          {games.map(game => (
+            <figure key={game.name}>
+               { game.name == 'CSGO' ? <img src="https://steamcdn-a.akamaihd.net/steam/apps/578080/library_600x900.jpg"/> : null }
+               { game.name == 'PUBG' ? <img src="https://steamcdn-a.akamaihd.net/steam/apps/578080/library_600x900.jpg"/> : null }
+            </figure>
+          ))}
+ </div>
 </div>
 </div>
     </div>
