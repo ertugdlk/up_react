7318d7a4a32d3e4b2e40587a26500ff365e914aa
Mert Mungan <business.mertmungan@gmail.com>
2020-11-27 12:13:50 +0300

    MyAccount prototype

--- src/components/LoginWindow.js
+++ src/components/LoginWindow.js
@@ -68,6 +68,7 @@ function LoginWindow()
               required
             />
             <Button onClick={handleLogin}>Login</Button>
+            <SteamButton><img src="https://upload.wikimedia.org/wikipedia/commons/8/83/Steam_icon_logo.svg"  height="20px"/>Login with Steam</SteamButton>
         </Modal>  
         </Window> 
         </>
@@ -131,9 +132,12 @@ const Button = styled.button`
   padding: 0.75em ;
   border: 2px solid #00ff60;
   border-radius: 8px;
-  width: fit-content;
-  height: fit-content;
-  margin-left:50px;
+  width: 100px;
+  height: 100%;
+  margin-left:25px;
+  display:flex;
+  align-items:center;
+  justify-content:center;
 
   &:hover {
       background-color: #16161b;
@@ -141,5 +145,26 @@ const Button = styled.button`
       border-color: #f1f1f1;
   }
 `;
+
+const SteamButton = styled.button`
+  color: #fff;
+  background-color: #1b1c23;
+  font-size: 1em;
+  margin-top:1.5em;
+  padding-top: 0.50em;
+  padding-bottom:0.50em;
+  border: 1px solid #1b1c23;
+  border-radius: 8px;
+  width: 250px;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  &:hover {
+      background-color: #16161b;
+      color: #f1f1f1;
+      border-color: #f1f1f1;
+  }
+`
 
 export default LoginWindow
