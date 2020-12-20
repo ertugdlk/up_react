e71104c6ca06cbbaf906347d4094085c1949fe2f
Ertug Dilek <ertgdlk@gmail.com>
2020-12-20 15:31:24 +0300

    baseurl change

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,4 +1,4 @@
-export const baseUrl = 'http://ec2-52-59-234-23.eu-central-1.compute.amazonaws.com:5000/';
+export const baseUrl = 'https://test.unknownpros.com/';
 export const fetchAllRooms = 'FETCH_ALL_ROOMS';
 export const addNewRoom = 'ADD_NEW_ROOM';
 export const getUserGames = 'GET_USER_GAMES';
