11116f9928233fcc106b9cc00ba7adb2c37e8663
Mert Mungan <business.mertmungan@gmail.com>
2021-01-19 16:41:16 +0300

    Room UI screen width refactor

--- src/components/Room.js
+++ src/components/Room.js
@@ -360,6 +360,7 @@ function Room(props) {
   return (
     <>
       <div className="room-window">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
         <Snackbar
           open={snackbar}
           anchorOrigin={{ vertical: "top", horizontal: "center" }}
