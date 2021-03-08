7a92c560c8fdf0d09a4a2264b2de663dd6ca54bc
Ömer EREN <erenomer@gmail.com>
2021-03-08 12:45:49 +0300

    MA Selection Done

--- src/components/MapSelection.js
+++ src/components/MapSelection.js
@@ -1,122 +1,127 @@
-import React, { useState,useEffect,useRef } from 'react'
+import React, { useState, useEffect, useRef, useCallback } from 'react'
 import ClearIcon from '@material-ui/icons/Clear'
 /* --------------------------------- HELPERS -------------------------------- */
 import axios from '../utils'
 /* -------------------------------------------------------------------------- */
-import css from "../components/css/MapSelection.css"
+import css from '../components/css/MapSelection.css'
 import CenterModal from './UI/CenterModal'
 import { set } from 'js-cookie'
 
 function MapSelection(props) {
+  const [maps, setMaps] = useState([])
+  const [team1FirstIndex, setTeam1FirstIndex] = useState('')
+  const [team2FirstIndex, setTeam2FirstIndex] = useState('')
+  const [gameInformation, setGameInformation] = useState([])
+  const [gameUrl, setGameUrl] = useState('')
 
-    const [maps, setMaps] = useState([])
-    const [filteredMaps,setFilteredMaps] = useState([])
-    const [team1FirstIndex,setTeam1FirstIndex] = useState("")
-    const [team2FirstIndex,setTeam2FirstIndex] = useState("")
-    const [gameInformation,setGameInformation] = useState([])
+  //IMPROVEMENT İLERİSİNN İŞİ
+  //banlanan mapler localstorage ta tutlup kontrol edilebilir. oda da başladığında local storage da roomid si ile güncel mapler tutulabilir
 
-    //IMPROVEMENT İLERİSİNN İŞİ 
-    //banlanan mapler localstorage ta tutlup kontrol edilebilir. oda da başladığında local storage da roomid si ile güncel mapler tutulabilir
-
-    const disableButton = (index) =>{
-      // socket io emit ban
-      const bannedMap = index;
-      props.socket.emit("mapselection", {host:props.host , bannedMap, team: 1})
-
-     }
-    
-    useEffect(() => {
-        async function GameMaps() {
-          const url = 'room/getmaps'
-          const response = await axios.post(url, {gameName:"CSGO"},{ withCredentials: true })
-          if (response.data) {       
-            setMaps(response.data.maps)
-          }
-        }
-
-      setTeam1FirstIndex(props.team1[0].nickname)
-      setTeam2FirstIndex(props.team2[0].nickname)
-
-        GameMaps()
-      }, [])
-
-      useEffect(() => {
-        let filteredItems = []
-        const url = 'steam://connect/' + gameInformation
-        props.socket.on('nextTurn', ({bannedMap, team}) => {
-          filteredItems = filteredItems.concat(maps[bannedMap])
-         
-          /*const filteredItem = maps.filter(banMap=>{
-            console.log("banned map", bannedMap)
-            console.log("remaining maps", maps.splice(bannedMap,1))
-            return banMap=== maps[bannedMap]})
-            console.log("filtered item",filteredItem)*/
-          if(filteredItems.length == 1){
-            return(
-            <div>
-              <span>{gameInformation}</span>
-              <a href={url} class="btn btn-primary">
-                {" "}
-                Join the Game
-              </a>
-          </div>
-          )
-          }
-          setFilteredMaps(filteredItems)
-          setMaps(maps.splice(bannedMap,1))
-        })
+  const disableButton = (index) => {
+    // socket io emit ban
+    if (maps.length !== 1) {
+      const bannedMap = index
+      props.socket.emit('mapselection', {
+        host: props.host,
+        bannedMap,
+        team: 1,
       })
+    }
+  }
 
-      useEffect(async () => {
-        
-          const url = 'rcon/setupmatch'
-          const response = await axios.post(
-            url,
-            { host: props.host },
-            { withCredentials: true }
-          )
-          setGameInformation(response.data)
+  const fetchMyGameInfo = useCallback(async () => {
+    const url = 'rcon/setupmatch'
+    const response = await axios.post(
+      url,
+      { host: props.host },
+      { withCredentials: true }
+    )
+
+    console.log('setupmatch', response)
+    setGameInformation(response.data)
+  }, [])
+
+  useEffect(() => {
+    if (maps.length === 1) {
+      fetchMyGameInfo()
+    }
+  }, [maps])
+
+  useEffect(() => {
+    if (maps.length === 0) {
+      async function GameMaps() {
+        const url = 'room/getmaps'
+        const response = await axios.post(
+          url,
+          { gameName: 'CSGO' },
+          { withCredentials: true }
+        )
+        if (response.data) {
+          setMaps(response.data.maps)
         }
-      )
+      }
+      GameMaps()
+    }
 
-      if(team1FirstIndex != "" || team2FirstIndex != "" ){
-        return(
-          <>
-          <div className="map-room-window">
-    <div class='wrapper'>
-    <div className='maps'>
-              {maps.map((map,index) => (
-                <li className="map-list">
-                <button
-                  className='map' onClick={()=> disableButton(index)}>
+    setTeam1FirstIndex(props.team1[0].nickname)
+    setTeam2FirstIndex(props.team2[0].nickname)
+  }, [])
+
+  useEffect(() => {
+    const url = 'steam://connect/' + gameInformation
+    props.socket.on('nextTurn', ({ bannedMap, team }) => {
+      setMaps(maps.splice(bannedMap, 1))
+    })
+  })
+
+  // useEffect(async () => {
+  //   const url = 'rcon/setupmatch'
+  //   const response = await axios.post(
+  //     url,
+  //     { host: props.host },
+  //     { withCredentials: true }
+  //   )
+
+  //   console.log('setupmatch', response)
+  //   setGameInformation(response.data)
+  // })
+
+  if ((team1FirstIndex != '' || team2FirstIndex != '') && maps.length !== 1) {
+    return (
+      <>
+        <div className='map-room-window'>
+          <div class='wrapper'>
+            <div className='maps'>
+              {maps.map((map, index) => (
+                <li className='map-list'>
+                  <button className='map' onClick={() => disableButton(index)}>
                     <span>{maps[index]}</span> {/*Buraya image gelecek*/}
-                </button>
+                  </button>
                 </li>
               ))}
+            </div>
+          </div>
+        </div>
+      </>
+    )
+  } else {
+    return (
+      <>
+        <div className='map-room-window'>
+          <div class='wrapper'>
+            <div className='maps'>
+              <div>
+                <span>{gameInformation}</span>
+                <a href={'url'} class='btn btn-primary'>
+                  Join the Game
+                </a>
               </div>
             </div>
+          </div>
         </div>
-          </>
-        )
-      }else{
-        return(
-        <>
-         <div className="map-room-window">
-    <div class='wrapper'>
-    <div className='maps'>
-             {filteredMaps.map((map) => (
-               <li className="map-list">
-                <button disabled className='disabled-map'>
-                    <span>Deneme 2</span> {/*Buraya image gelecek*/}
-                </button>
-                </li>
-              ))}
-            </div>
-            </div>
-    </div>
-        </>
-        )
-      }
+      </>
+    )
+  }
 }
 
-export default MapSelection;
+export default MapSelection
